9991bda749f71610b9c58a765f8c63fba2f95399
Daniel Vassilev <dunyto@gmail.com>
2024-11-20 16:24:23 +0100

    Added processFunction.ts, ttl and uuid.

--- lib/prep-stack.ts
+++ lib/prep-stack.ts
@@ -5,6 +5,8 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
+import { CfnOutput } from 'aws-cdk-lib';
+import { Topic, Subscription, SubscriptionProtocol } from 'aws-cdk-lib/aws-sns';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PrepStack extends cdk.Stack {
@@ -16,17 +18,39 @@ export class PrepStack extends cdk.Stack {
         name: 'id', 
         type: dynamodb.AttributeType.STRING 
       },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      timeToLiveAttribute: 'ttl'
+    });
+
+    const errorTopic : Topic = new Topic(this, 'ErrorTopic', { 
+      topicName: 'ErrorTopic'
     });
 
     const processFunction = new NodejsFunction(this, 'ProcessFunction', {
       runtime: Runtime.NODEJS_20_X,
       entry: `${__dirname}/../src/processFunction.ts`,
       handler: 'handler',
+      environment: {
+        TABLE_NAME: errorTable.tableName,
+        TOPIC_ARN: errorTopic.topicArn
+      }
     });
+
+    errorTable.grantReadWriteData(processFunction);
+    errorTopic.grantPublish(processFunction);
 
     const api = new RestApi(this, 'ProcessorApi');
     const resource = api.root.addResource('processJSON');
     resource.addMethod('POST', new LambdaIntegration(processFunction));
+
+    new Subscription(this, 'ErrorSubscription', {
+      topic: errorTopic,
+      protocol: SubscriptionProtocol.EMAIL,
+      endpoint: 'dunyto@etik.com'
+    });
+
+    new CfnOutput(this, 'RESTApiEndpoint', {
+      value: `https://${api.restApiId}.execute-api.${this.region}.amazonaws.com/prod/processJSON`
+    });
   }  
 }
