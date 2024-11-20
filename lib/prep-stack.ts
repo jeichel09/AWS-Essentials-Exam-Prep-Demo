2ad8bfc784a2c0f25a41176dfac6fb8ec9e3556b
Daniel Vassilev <dunyto@gmail.com>
2024-11-20 11:24:37 +0100

    Added DynamoDB-Table, processFunction and API to prep-stack.

--- lib/prep-stack.ts
+++ lib/prep-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PrepStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const errorTable : dynamodb.Table = new dynamodb.Table(this, 'ErrorTable', {
+      partitionKey: { 
+        name: 'id', 
+        type: dynamodb.AttributeType.STRING 
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PrepQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const processFunction = new NodejsFunction(this, 'ProcessFunction', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: `${__dirname}/../src/processFunction.ts`,
+      handler: 'handler',
+    });
+
+    const api = new RestApi(this, 'ProcessorApi');
+    const resource = api.root.addResource('processJSON');
+    resource.addMethod('POST', new LambdaIntegration(processFunction));
+  }  
 }
---
+++ src/processFunction.ts
@@ -0,0 +1,5 @@
+export const handler = async (event: any) => {
+    return {
+        statusCode: 200,
+    };
+};
